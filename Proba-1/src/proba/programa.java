b95a4923647018f5de53a1d2f9fb1496e43c6ad6
joseMTB-D <josemtbt@gmail.com>
2020-09-17 10:49:27 +0200

    17/09/2020

--- Proba-1/src/proba/programa.java
+++ Proba-1/src/proba/programa.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class programa {
 	public static void main (String[]args) {
 		Scanner scan= new Scanner(System.in);
-		int n1=0,n2=0;
+		/*int n1=0,n2=0;
 		System.out.println("Inserta 2 numeros");
 		n1=scan.nextInt();
 		n2=scan.nextInt();
@@ -19,6 +19,27 @@ public class programa {
 		}else {
 			System.out.println("Resultat no valid");
 		}
+		*/
+		/*
+		System.out.println("Inserta el numero de alumnes:");
+		int al=scan.nextInt();
+		scan.nextLine();	
+		
+		String [] alumnes = new String[al];
+		for(int i =0;i<alumnes.length;i++) {
+			System.out.println("Inserta el nom del alumne "+(i+1));
+			alumnes[i]=scan.nextLine();	
+		}
+		int c=0;
+		while(c<(al)){
+			System.out.println("L'alumne numero "+(c+1)+" Es diu: "+alumnes[c]);
+		 	c+=1;
+		}
+		*/
+		
+		
+		
+		
 		
 		
 		
