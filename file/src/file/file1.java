e16c2bb28ec1140a21a4ef33d42f207616c03781
joseMTB-D <josemtbt@gmail.com>
2020-09-23 11:43:26 +0200

    23/09/2020\\11:43

--- file/src/file/file1.java
+++ file/src/file/file1.java
@@ -1,10 +1,26 @@
 package file;
 
+import java.io.File;
+import java.io.IOException;
+
 public class file1 {
 
 	public static void main(String[] args) {
-		File f= new File();
+		File f1= new File("FEDERICO3.txt");
+		File f2= new File("A:\\Estudis\\DAM-2\\Acces_a_Dades\\UF1\\M6\\file","FEDERICO3.txt");
+		File ruta= new File("A:\\Estudis\\DAM-2\\Acces_a_Dades\\UF1\\M6\\file");
+		File f4 = new File(ruta,"FEDERICO3.txt");
 
+		
+		try {
+			System.out.print(f2.createNewFile());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 
 }
